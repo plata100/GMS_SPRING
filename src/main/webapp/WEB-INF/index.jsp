ea9d88bd4c0dfd4218d1c531055c8787432c56d2
plata100 <plata100@daum.net>
2017-09-19 11:56:19 +0900

    20170919 Morning

--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -3,18 +3,22 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-<meta http-equiv="Content-Type" content="text/html; charset=utl-8;" />
 <title>GMS</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="${path.css}/meta.css" />
+	<link rel="stylesheet" href="${path.css}/common.css" />
+	<link rel="stylesheet" href="${path.css}/member.css" />
 	<script src="${path.js}/meta.js"></script>
 </head>
 <body>
-<div id="wrapper"></div>
+<div id="wrapper">
+	<div id="navbar"></div>
+	<div id="container"></div>
+</div>
 </body>
-</html>
 <script>
 meta.common.init('${path.ctx}');
 </script>
+</html>
--- src/main/webapp/resources/js/meta.js
+++ src/main/webapp/resources/js/meta.js
@@ -1,8 +1,4 @@
-/*
- *
- */
 var meta=meta || {};
-
 meta.common=(function(){
 	var init=function(ctx){
 		onCreate();
@@ -12,60 +8,376 @@ meta.common=(function(){
 	var onCreate=function(){
 		setContentView();
 	};
-	var setContentView=function(){
-		
+	var setContentView=function(){};
+	return { init:init };
+})();
+meta.index=(function(){
+	var $wrapper,$navbar,$container,ctx,img,js,css;
+	var init=function(){
+		onCreate();
 	};
-	return{
+	var onCreate=function(){
+			setContentView();
+			$('#btn').on('click',function(){
+				$container.empty();
+				//meta.auth.init();	
+				meta.ui.navbar();
+				meta.navbar.init();
+			});
+		};
+	var setContentView=function(){
+		$container=$('#container');
+		img=$$('i');
+		var $image = $('<img/>',
+			{	
+				id : 'loading',
+				src : img+'/loading.gif'
+			}
+		);
+		$container.append($image);
+		var $btn=$('<input/>',
+				{
+					id : 'btn',
+					type : 'button',
+					value : '버튼'
+				});
+		$container.append($btn);
+	};
+	return {init:init};
+})();
+meta.algo={
+	arithmetic : function(s,e){
+		var sum=0;
+		var start=s*1;
+		var end=e*1;
+		for(var i=start;i<=end;i++){
+			sum+=i;
+		}
+		return sum;
+	},
+	switchSeries : ()=>{
+		// 1 - 2 + 3 - 4 + ... + 100
+		var sum=0;
+		var i=0;
+		var sw=0;
+		do{
+			i++;
+			if(sw==0) {
+				sum+=i;
+				sw=1;
+			} else {
+				sum-=i;
+				sw=0;
+			}
+		} while(i<100);
+		return sum;
+	},
+	differenceSeries : x=>{
+		var i=0;
+		var j=1;
+		var sum=1;
+		var limit=x*1;
+		do {
+			i++;
+			j+=i;
+			sum+=j;
+		} while (i<limit);
+		return sum;
+	},
+	factorialSeries : x=> {
+		var i=1, j=1, sum=1;
+		var limit=x*1;
+		do {
+			i++;
+			j*=i;
+			sum+=j;
+		} while(i<limit);
+		return sum;
+	},
+	fibonacciSeries : x=> {
+		var hap, cnt, c;
+		var a=1, b=1;
+		var limit=x*1;
+		hap=2;
+		cnt=2;
+		while(1) {
+			c=a+b;
+			hap+=c;
+			cnt++;
+			if(cnt<limit) {
+				a=b;
+				b=c;
+			} else {
+				return hap;
+				break;
+			}
+		}
+		
+	}
+};
+meta.auth=(function(){
+	var $wrapper,ctx,img,js,css;
+	var init=function(){
+		onCreate();
+	};
+	var onCreate=function(){setContentView();};
+	var setContentView=function(){
+		$wrapper=$('#wrapper');
+		img=$$('i');
+		loginView();
+	};
+	var loginView=function(){
+		
+		var ui=
+			'<div id="container">'
+			+'<div id="login_box">'
+			+	'<img src="'+img+'/login.jpg" alt="" /><br />'
+			+	'<span id="login_id">ID</span>'
+			+	'<input id="login_input" type="text"  /> <br />'
+			+	'<span id="login_pass">PASSWORD</span> '
+			+	'<input type="text"  /> <br />'
+			+'</div>'
+		+'</div>';
+		$wrapper.append(ui);
+		$('#login_input').after(meta.comp.input(
+			{
+				type : 'button',
+				id : 'login_btn',
+				value : '로그인'
+			}
+		));
+		$('#login_box').append(meta.comp.input(
+			{
+				type : 'button',
+				id : 'cancel_btn',
+				value : '취소'
+			}
+		));
+	};
+	var joinView=function(){};
+	return {
 		init : init
 	};
 })();
-
-var $$=function(){return meta.session.getPath('ctx');};
-var js=function(){return meta.session.getPath('js');};
-var css=function(){return meta.session.getPath('css');};
-var img=function(){return meta.session.getPath('img');};
-
-meta.session=(function(){
-	var init=function(ctx){
-		sessionStorage.setItem('ctx',ctx);
-		sessionStorage.setItem('js',ctx+'/resources/js');
-		sessionStorage.setItem('img',ctx+'/resources/img');
-		sessionStorage.setItem('css',ctx+'/resources/css');
+meta.navbar=(function(){
+	var init=function(){
+		onCreate();
 	};
-	var getPath=function(x){
-		return sessionStorage.getItem(x);
-	};
-	return {init:init,getPath:getPath};
-})();
-
-meta.index=(function(){
-	var $wrapper;
-	var init=function(){onCreate();};
 	var onCreate=function(){
 		setContentView();
-		$('#btn').on('click', function(){
-			alert('버튼 클릭'); 
-			$wrapper.empty();
+		$('.dropdown-menu a').eq(0).on('click',function(){
+			app.controller.moveTo('member','member_add');
 		});
+		$('.dropdown-menu a').eq(1).on('click',function(){
+			app.member.list(1);
+		});
+		$('.dropdown-menu a').eq(2).on('click',function(){
+			app.controller.moveTo('member','member_detail');
+		});
+		$('.dropdown-menu a').eq(3).on('click',function(){
+			
+		});
+		$('.dropdown-menu a').eq(4).on('click',function(){
+			app.controller.moveTo('grade','grade_add');
+		});
+		$('.dropdown-menu a').eq(5).on('click',function(){
+			app.controller.moveTo('hong','grade','grade_list');
+		});
+		$('.dropdown-menu a').eq(6).on('click',function(){
+			app.controller.moveTo('grade','grade_detail');
+		});
+		$('.dropdown-menu a').eq(7).on('click',function(){
+			
+		});
+		$('.dropdown-menu a').eq(8).on('click',function(){
+			app.controller.moveTo('board','board_write');
+		});
+		$('.dropdown-menu a').eq(9).on('click',function(){
+			app.controller.moveTo('board','board_list');
+		});
+		$('.dropdown-menu a').eq(10).on('click',function(){
+			app.controller.moveTo('board','board_detail');
+		});
+		$('.dropdown-menu a').eq(11).on('click',function(){
+			
+		});
+		$('#arithBtn').click(()=>{
+			$('#container').empty();
+			meta.ui.series();
+			$('#resultBtn').click(()=>{
+				$('#result').text('결과 : '+meta.algo.arithmetic($('#start').val(),$('#end').val()));
+			});
+		});
+		$('#switchBtn').click(()=>{
+			$('#container').empty();
+			meta.ui.series();
+			$('h1').html('SwitchSeries의 합');
+			$('#start').val('0').attr('readonly','true');
+			$('#end').attr('value','100').attr('readonly','true');
+			$('#resultBtn').click(()=>{
+				$('#result').text('결과 : '+meta.algo.switchSeries());;
+			});
+		});
+		$('#differBtn').click(()=>{
+			$('#container').empty();
+			meta.ui.series();
+			$('h1').html('DifferenceSeries의 합');
+			$('#start').val('1').attr('readonly','true');
+			$('#resultBtn').click(()=>{
+				$('#result').text('결과 : '+meta.algo.differenceSeries($('#end').val()));;
+			});
+		});
+		$('#facBtn').click(()=>{
+			$('#container').empty();
+			meta.ui.series();
+			$('h1').html('DifferenceSeries의 합');
+			$('#start').val('1').attr('readonly','true');
+			$('#resultBtn').click(()=>{
+				$('#result').text('결과 : '+meta.algo.factorialSeries($('#end').val()));;
+			});
+		});
+		$('#fiboBtn').click(()=>{
+			$('#container').empty();
+			meta.ui.series();
+			$('h1').html('FibonacciSeries의 합');
+			$('#start').val('1').attr('readonly','true');
+			$('#resultBtn').click(()=>{
+				$('#result').text('결과 : '+meta.algo.fibonacciSeries($('#end').val()));;
+			});
+		});
+		
+	};
+	var setContentView=function(){
+		var $u1=$("#navbar_ul_stu");
+		var $u2=$("#navbar_ul_grade");
+		var $u3=$("#navbar_ul_board");
+		$u1.addClass("dropdown-menu");
+		$u2.addClass("dropdown-menu");
+		$u3.addClass("dropdown-menu");
+	};
+	return {init:init};
+})();
+meta.ui=(function(){
+	var $wrapper,$navbar,$container,ctx,img,js,css;
+	var init=function(){
+		$wrapper=$('#wrapper');
+		$navbar=$('#navbar');
+		$container=$('#container');
+		img=$$('i');
+	};
+	var navbar = function(){
+		$('#navbar').html(
+		'<nav class="navbar navbar-inverse">'
+		+'  <div class="container-fluid">'
+		+'    <div class="navbar-header">'
+		+'      <a class="navbar-brand" href="#">GMS</a>'
+		+'    </div>'
+		+'    <ul class="nav navbar-nav">'
+		+'      <li class="active"><a ><span class="glyphicon glyphicon-home"></span>&nbsp;Home</a></li>'
+		+'      <li class="dropdown">'
+		+'          <a href="#" class="dropdown-toggle" '
+		+'          	aria-haspopup="true" '
+		+'          	aria-expanded="false">회원관리 <span class="caret">'
+		+'          	</span></a>'
+		+'          <ul id="navbar_ul_stu" class="dropdown-menu">'
+		+'            <li><a>학생추가</a></li>'
+		+'            <li><a>학생목록</a></li>'
+		+'            <li><a>학생조회</a></li>'
+		+'            <li></li>'
+		+'            <li><a>학생삭제</a></li>'
+		+'          </ul>'
+		+'        </li>'
+		+'      <li class="dropdown">'
+		+'          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">성적관리 <span class="caret"></span></a>'
+		+'          <ul id="navbar_ul_grade" class="dropdown-menu">'
+		+'            <li><a>성적추가</a></li>'
+		+'            <li><a>성적목록</a></li>'
+		+'            <li><a>성적조회</a></li>'
+		+'            <li></li>'
+		+'            <li><a>성적삭제</a></li>'
+		+'          </ul>'
+		+'        </li>'
+		+'      <li class="dropdown">'
+		+'          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">게시판관리 <span class="caret"></span></a>'
+		+'          <ul id="navbar_ul_board" class="dropdown-menu">'
+		+'           <li><a>게시글추가</a></li>'
+		+'            <li><a>게시글목록</a></li>'
+		+'            <li><a>게시글조회</a></li>'
+		+'            <li></li>'
+		+'            <li><a>게시글삭제</a></li>'
+		+'          </ul>'
+		+'        </li>'
+		+'      <li class="dropdown">'
+		+'          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">수 열 <span class="caret"></span></a>'
+		+'          <ul id="navbar_ul_board" class="dropdown-menu">'
+		+'           <li><a id="arithBtn">등차수열</a></li>'
+		+'            <li><a id="switchBtn">스위치수열</a></li>'
+		+'            <li><a id="differBtn">계차수열</a></li>'
+		+'            <li><a id="facBtn">팩토리얼</a></li>'
+		+'            <li><a id="fiboBtn">피보나치</a></li>'
+		+'          </ul>'
+		+'        </li>'
+		+'    </ul>'
+		+'    <span class="float-right">${user.name} &nbsp;'
+		+'    	<a id="logout" >로그아웃</a></span>'
+		+'  </div>'
+		+'</nav>');
+	}
+	
+	var series=function(){
+		/*1부터 100까지 등차수열의 합*/
+		var content = '<div id="content">'
+			+'<h1>등차수열의 합</h1>'
+			+'<span id="start_txt">시작값: &nbsp;&nbsp;</span>'
+			+'<br/><span id="end_txt">끝   값:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>'
+			+'<div id="result"></div>';
+		$('#container').append(content);
+		$('#start_txt').after(meta.comp.input(
+				{
+					type:'text',
+					id: 'start',
+					placeholder : '시작값'
+				}
+			));
+		$('#end_txt').after(meta.comp.input(
+				{
+					type:'text',
+					id: 'end',
+					placeholder : '끝값'
+				}
+			));
+		$('#result').before(meta.comp.input(
+				{
+					type:'button',
+					id: 'resultBtn',
+					value : '결과보기'
+				}
+		));
 	};
 	
-	var setContentView=function(){
-		$wrapper=$('#wrapper');
-		var $image=$('<img/>',
-			{
-				id:'loading',
-				src:img()+'/loading.gif'
-			}
-		);
-		$wrapper.append($image);
-		var $btn=$('<input/>',
-			{
-				id:'btn',
-				type:'button',
-				value:'버튼'
-			}
-		);
-		$($wrapper).append($btn); //btn.appendTo(wrapper);와 같은 의미
+	return {
+		init : init,
+		series : series,
+		navbar : navbar
 	};
-	return {init : init};
 })();
+meta.comp=
+	{
+		input : function(json){
+			return $('<input/>',json);
+		}
+	};
+
+meta.session=
+	{
+	init : function(x){
+				sessionStorage.setItem('x',x);
+				sessionStorage.setItem('j',x+'/resources/js');
+				sessionStorage.setItem('c',x+'/resources/css');
+				sessionStorage.setItem('i',x+'/resources/img');
+		   },
+	getPath : function(x){
+				return sessionStorage.getItem(x);
+		   }
+	};
+var $$= function(x){return meta.session.getPath(x);};
